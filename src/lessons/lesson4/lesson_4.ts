3982f9613ed50dec99ed622294f762335fc38e4a
yaroslav-tormasov <yaroslav606@gmail.com>
2021-01-31 15:11:25 +0400

    lesson 4

--- src/lessons/lesson4/lesson_4.ts
+++ src/lessons/lesson4/lesson_4.ts
@@ -3,22 +3,233 @@ console.log('lesson 4');
 // http://latentflip.com/loupe/?code=JC5vbignYnV0dG9uJywgJ2NsaWNrJywgZnVuY3Rpb24gb25DbGljaygpIHsKICAgIHNldFRpbWVvdXQoZnVuY3Rpb24gdGltZXIoKSB7CiAgICAgICAgY29uc29sZS5sb2coJ1lvdSBjbGlja2VkIHRoZSBidXR0b24hJyk7ICAgIAogICAgfSwgMjAwMCk7Cn0pOwoKY29uc29sZS5sb2coIkhpISIpOwoKc2V0VGltZW91dChmdW5jdGlvbiB0aW1lb3V0KCkgewogICAgY29uc29sZS5sb2coIkNsaWNrIHRoZSBidXR0b24hIik7Cn0sIDUwMDApOwoKY29uc29sZS5sb2coIldlbGNvbWUgdG8gbG91cGUuIik7!!!PGJ1dHRvbj5DbGljayBtZSE8L2J1dHRvbj4%3D
 // https://jakearchibald.com/2015/tasks-microtasks-queues-and-schedules/
 
+// Task 1
+// setTimeout(()=> console.log(1), 0);
+// console.log(2);
+// (() => console.log(3))();
+// Promise.resolve(console.log(4));
+
+// Task 2
+// new Promise((res, rej) => {
+//   console.log(1);
+// })
+// new Promise((res, rej) => {
+//   setTimeout(()=> console.log(2), 0);
+// })
+// Promise.resolve(setTimeout(()=> console.log(3), 0));
+// console.log(4);
+// Promise.reject(console.log(5));
+
+// Task 3
+// (function(){
+//   setTimeout(()=> console.log(1), 100);
+// })();
+// console.log(2);
+// new Promise((res, rej) => {
+//   setTimeout(()=> console.log(3), 50);
+// })
+// function f() {
+//   console.log(4);
+// }
+// Promise.resolve(console.log(5));
+
+//Task 4
+
+// function f(num:number) {
+//   console.log(num);
+// }
+// Promise.resolve(1)
+//   .then(f);
+// (function(){
+//   console.log(2);
+// })();
+// console.log(3);
+// new Promise((res, rej) => {
+//   console.log(4);
+// });
+// setTimeout(f, 0, 5);
+
+// Task 5
+
+// console.log(1);
+// function f() {
+//   console.log(2);
+// }
+// setTimeout(()=>{
+//   console.log(3);
+//   let p = new Promise((res, rej) => {
+//     console.log(4);
+//     res();
+//   });
+//   p.then(() => f())
+// },0);
+// let l = new Promise((res, rej) => {
+//   console.log(5);
+//   rej();
+// });
+// l.then(res => console.log(res)).catch(() => console.log(6));
+// console.log(7);
+
+// Task 6
+
+// async function sleep(ms: number) {
+//   return new Promise((res, rej) => {
+//     setTimeout(() => {
+//       console.log(ms);
+//       res();
+//     }, ms*100);
+//   });
+// }
+
+// async function sleep(ms: number) {
+//   return new Promise((res) => {
+//     setTimeout(() => {
+//       res(console.log(ms));
+//     }, ms*100);
+//   })
+// }
+//
+// async function show() {
+//   await sleep(3)
+//   await sleep(2)
+//   await sleep(1)
+// }
+//
+// show();
+
+//Task 7
+// let pr1 = new Promise((res) => {
+//   res(10);
+// });
+// let pr2 = new Promise((res) => {
+//   res(0)
+// });
+// pr1
+//   .then((res: any) => {
+//     console.log(res);
+//     return res + 2;
+//   })
+//   .then((res: any) => {
+//     console.log(res);
+//     return res + 2;
+//   })
+//   .then(console.log);
+// pr2
+//   .then((res: any) => {
+//     console.log(res);
+//     return res + 1;
+//   })
+//   .then((res: any) => {
+//     console.log(res);
+//     return res + 1;
+//   })
+//   .then(console.log);
+
+
+// const pr = new Promise((res, rej) => {
+//   // syncing
+//   // ajax, ...
+//   res();
+//   rej();
+// });
+// pr
+//   .then((res) => {
+//     //...
+//     return 10;
+//   },
+//     (rej) => {
+//     //...
+//       return 0;
+//     })
+//   .catch((rej) => {
+//     //...
+//     return 0;
+//   })
+//   .finally()
+//   .then()
+//   .catch()
+
+// (async function f() {
+//   try {
+//     // ....
+//     const result = await new Promise((res, rej) => {
+//       // ajax
+//       res(10);
+//     })
+//      // ...
+//     return 50;
+//   } catch (e) {
+//     // execute error case
+//   }
+// })()
+//   .then()
+//   .catch()
 
 // Task 01
 // Создайте промис, который постоянно находиться в состоянии pending.
 // В конструкторе промиса выведите в консоль сообщение "Promise is created".
 
+// const task1 = new Promise((res, rej) => {
+//   console.log("Promise is created");
+// })
+//
+// console.log(task1);
 
 // Task 02
 // Создайте промис, который после создания сразу же переходит в состояние resolve
 // и возвращает строку 'Promise Data'
 // Получите данные промиса и выведите их в консоль
 
+// const task2 = new Promise((res, rej) => {
+//   res('Promise Data');
+// })
+// task2.then(r => console.log(r));
+//task2.then(console.log);
+
+// const task2 = Promise.resolve('Promise Data');
+// task2.then(r => console.log(r));
+
+//Promise.resolve('Promise Data').then(r => console.log(r));
+
+// async function testTask2() {
+//   return 'Promise Data';
+// }
+// testTask2().then(console.log);
 
 // Task 03
 // Создайте промис, который после создания сразу же переходит в состояние rejected
 // и возвращает строку 'Promise Error'
 // Получите данные промиса и выведите их в консоль
+
+// const task3 = new Promise((res, rej) => {
+//   rej('Promise Error');
+// })
+// task3.then(null, err => console.log(err));
+// task3.then(null, console.log);
+// task3.catch(console.log);
+
+// const task3 = Promise.reject('Promise Error');
+// task3.catch(console.log);
+
+// Promise.reject('Promise Error').catch(console.log);
+
+// async function testTask3() {
+//   throw 'Promise Error';
+// }
+
+// (async function() {
+//   // return Promise.reject('Promise Error');
+//   await new Promise ((res, rej) => {
+//     rej('Promise Error');
+//   })
+//   return 10;
+// })().catch(console.log)
+
+// async function testTask3() {
+//   await Promise.reject('Promise Error');
+// }
+//
+// testTask3().catch(console.log);
 
 
 // Task 04
@@ -26,6 +237,11 @@ console.log('lesson 4');
 // (Используйте setTimeout)
 // и возвращает строку 'Promise Data'
 // Получите данные промиса и выведите их в консоль
+
+// const testTask4 = new Promise((res, rej) => {
+//   setTimeout(res, 3000, 'Promise Data');
+// });
+// testTask4.then(console.log)
 
 
 // Task 05
@@ -41,6 +257,48 @@ console.log('lesson 4');
 // свойства resolve и reject получают ссылки на соответствующие функции
 // resolve и reject. Следующие два обработчика запускают методы resolve и reject.
 
+type testObjType = {
+  promise: null | Promise<any>;
+  resolve: null | Function;
+  reject: null | Function;
+  onSuccess: (paramName: string) => void;
+  onError: (paramName: string) => void;
+}
+
+const handlePromise: testObjType = {
+  promise: null,
+  resolve: null,
+  reject: null,
+  onSuccess: (paramName: string) => {
+    console.log(`Promise is resolved with data: ${paramName}`);
+  },
+  onError:(paramName: string) => {
+    console.log(`Promise is rejected with error: ${paramName}`);
+  },
+}
+
+export const createPromise = () => {
+  const somePromise: Promise<any> = new Promise((res, rej) => {
+    handlePromise.resolve = res;
+    handlePromise.reject = rej;
+  });
+  handlePromise.promise = somePromise;
+  handlePromise.promise
+    .then(res => handlePromise.onSuccess(res))
+    .catch(err => handlePromise.onError(err));
+  console.log(handlePromise);
+}
+
+export const resolvePromise = () => {
+  handlePromise.resolve && handlePromise.resolve('1');
+}
+
+export const rejectPromise = () => {
+  handlePromise.reject && handlePromise.reject('0');
+}
+
+// @ts-ignore
+window.handlePromise = handlePromise;
 
 // Task 06
 // Создайте промис, который через 1 с возвращает строку "My name is".
@@ -49,6 +307,22 @@ console.log('lesson 4');
 // Создайте функцию print, которая выводит в консоль значение своего параметра
 // Добавьте два метода then и передайте созданные функции.
 
+function print(value: string): void {
+  console.log(value);
+}
+
+function onSuccess(value: string): string {
+  return `${value} Eugen`;
+}
+
+// const myPromiseName: Promise<string> = new Promise<string>((res, rej) => {
+//   setTimeout(res, 3000, "My name is");
+// });
+//
+// myPromiseName
+//   .then(onSuccess)
+//   .then(print)
+
 
 // Task 7
 // Создайте три промиса. Первый промис возвращает объект { name: "Anna" } через 2с,
@@ -56,6 +330,21 @@ console.log('lesson 4');
 // Получите результаты работы промисов, объедините свойства объектов
 // и выведите в консоль {name, age, city}
 
+
+let prom1: Promise<Object> = new Promise<Object>(res => {
+  setTimeout(res, 2000, { name: "Anna" })
+});
+
+let prom2: Promise<Object> = new Promise<Object>(res => {
+  setTimeout(res, 3000, { age: 16 })
+})
+
+let prom3: Promise<Object> = new Promise<Object>(res => {
+  setTimeout(res, 4000, { city: 'Minsk' })
+})
+
+let result: Promise<Array<Object>> = Promise.all([prom1, prom2, prom3]);
+result.then(([a,b,c]: Array<Object>) => ({...a, ...b, ...c})).then(console.log);
 
 
 // just a plug
